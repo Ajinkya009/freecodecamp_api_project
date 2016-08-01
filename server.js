e2288e24ebb96db9d64475a9806543b54c8346bb
Ajinkya Deshmukh <ajinkyadeshmukh9@gmail.com>
2016-08-01 06:50:15 +0000

    final commit

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 var express = require("express");
+var moment = require("moment");
+var path = require("path");
+var morgan = require("morgan");
 var app = express();
-app.get('/',function(req,res){
-    res.writeHeader(200,{'Content-Type':'text/plain'});
-    res.end("Hello World");
+app.use(morgan('dev'));
+app.use(express.static(path.resolve(__dirname,'front_end')));
+
+app.get('/:timestring',function(req,res){
+    
+    if(moment(req.params.timestring,'MMMM DD, YYYY',true).isValid()){
+        var value = moment(req.params.timestring,'MMMM DD, YYYY');
+        res.json({
+      unix: value.format("X"),
+      natural: value.format("MMM DD, YYYY")
+    });
+    }
+    else if(moment(req.params.timestring,'X',true).isValid()){
+       var value = moment(req.params.timestring,'X');
+        res.json({
+      unix: value.format("X"),
+      natural: value.format("MMM DD, YYYY")
+    });
+    }
+    else{
+        res.json({
+      unix: null,
+      natural: null
+    });
+    }
+    
 });
+
 app.listen(process.env.PORT,function(){
-    console.log("Server listening at port"+process.argv[2]);
+    console.log("Server listening at port:"+process.env.PORT);
 });
